21f96e19ab4151de493648570a3929ed00034953
Sardi Kapilano <sardikapilano24@gmail.com>
2018-05-07 14:16:33 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 date_default_timezone_set("Asia/Jakarta");
 
-define('DB_HOST', '192.168.35.161');
-define('DB_USERNAME', 'test');
-define('DB_PASSWORD', '123123');
-define('DB_NAME', 'db_test');
+define('DB_HOST', 'sfa.pma.tk');
+define('DB_USERNAME', 'sar');
+define('DB_PASSWORD', 's4r');
+define('DB_NAME', 'sar');
 // define('BASE_URL', 'https://sis.herokuapp.com/');
 
 ?>
