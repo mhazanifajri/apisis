0afe5e4834064d9981404204cb2dc5195238736a
Sardi Kapilano <sardikapilano24@gmail.com>
2018-05-07 11:42:02 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 date_default_timezone_set("Asia/Jakarta");
 
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'db_sistem_informasi_siswa');
-define('BASE_URL', 'http://localhost/sis/');
+define('DB_HOST', '192.168.35.161');
+define('DB_USERNAME', 'test');
+define('DB_PASSWORD', '123123');
+define('DB_NAME', 'db_test');
+define('BASE_URL', 'https://sis.herokuapp.com/');
 
 ?>
