369d5b12557f3842d524c77d921d074a159bcc60
Sardi Kapilano <sardikapilano24@gmail.com>
2018-05-07 11:46:56 +0700

    Update config.php

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ define('DB_HOST', '192.168.35.161');
 define('DB_USERNAME', 'test');
 define('DB_PASSWORD', '123123');
 define('DB_NAME', 'db_test');
-define('BASE_URL', 'https://sis.herokuapp.com/');
+// define('BASE_URL', 'https://sis.herokuapp.com/');
 
 ?>
