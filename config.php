a86084c94d9331830e715214fcbac884ec53dc11
Sardi Kapilano <sardikapilano24@gmail.com>
2018-05-09 13:59:18 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,9 @@
 
 date_default_timezone_set("Asia/Jakarta");
 
-define('DB_HOST', 'mirrorsfa.tk');
-define('DB_USERNAME', 'sar');
-define('DB_PASSWORD', 's4r');
-define('DB_NAME', 'sar');
+define('DB_HOST', 'sql12.freesqldatabase.com');
+define('DB_USERNAME', 'sql12237101');
+define('DB_PASSWORD', '3P3zdMvkdE');
+define('DB_NAME', 'sql12237101');
 define('BASE_URL', 'https://sisinfo.herokuapp.com/');
-
 ?>
