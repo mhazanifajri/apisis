68f2ef7d4e3fbfb0c712e6f262719fd608595484
Sardi Kapilano <sardikapilano24@gmail.com>
2018-05-17 14:01:30 +0700

    Update file_upload.php

--- file_upload.php
+++ file_upload.php
@@ -5,12 +5,13 @@ ServerConfig();
 $PdfUploadFolder = 'files/';
  
 $ServerURL = 'https://sisinfo.herokuapp.com/'.$PdfUploadFolder;
+define ('SITE_ROOT', realpath(dirname(__FILE__)));
  
 if($_SERVER['REQUEST_METHOD']=='POST'){
  
     if(isset($_POST['name']) and isset($_FILES['pdf']['name'])){
 
-	$con = mysqli_connect(HostName,HostUser,HostPass,DatabaseName);
+    $con = mysqli_connect(HostName,HostUser,HostPass,DatabaseName);
 		
         $PdfName = $_POST['name'];
 		
@@ -20,13 +21,15 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
  
         $PdfFileURL = $ServerURL . GenerateFileNameUsingID() . '.' . $PdfFileExtension;
  
-        $PdfFileFinalPath = $PdfUploadFolder . GenerateFileNameUsingID() . '.'. $PdfFileExtension;
+        $PdfFileFinalPath = $ServerURL . GenerateFileNameUsingID() . '.'. $PdfFileExtension;
+
+        $uploadfile = $_SERVER['DOCUMENT_ROOT'] . '/sis/files/'.$PdfName.'.'.$PdfFileExtension;
+
  
         try{
+            move_uploaded_file($_FILES['pdf']['tmp_name'],$uploadfile);
 			
-            move_uploaded_file($_FILES['pdf']['tmp_name'],$PdfFileFinalPath);
-			
-            $InsertTableSQLQuery = "INSERT INTO tbl_modul (id_topic_modul, nama_modul, tgl_dibuat, link_file) VALUES ('1', 'ngising', '2018-09-12','$PdfName') ;";
+            $InsertTableSQLQuery = "INSERT INTO tbl_modul (id_topic_modul, nama_modul, tgl_dibuat, link_file) VALUES ('1', 'ngising', '2018-09-12','files/$PdfName') ;";
 
             mysqli_query($con,$InsertTableSQLQuery);
 
@@ -42,6 +45,11 @@ define('HostName','mirrorsfa.tk');
 define('HostUser','sar');
 define('HostPass','s4r');
 define('DatabaseName','sar');
+
+// define('HostName', 'localhost');
+// define('HostUser', 'root');
+// define('HostPass', '');
+// define('DatabaseName', 'db_sistem_informasi_siswa');
 	
 }
 
@@ -49,19 +57,19 @@ function GenerateFileNameUsingID(){
     
 	$con2 = mysqli_connect(HostName,HostUser,HostPass,DatabaseName);
 	
-	$GenerateFileSQL = "SELECT max(id) as id FROM tbl_modul";
+	$GenerateFileSQL = "SELECT max(id_modul) as id_modul FROM tbl_modul";
 	
     $Holder = mysqli_fetch_array(mysqli_query($con2,$GenerateFileSQL));
 
     mysqli_close($con2);
 	
-    if($Holder['id']==null)
+    if($Holder['id_modul']==null)
 	{
         return 1;
 	}
     else
 	{
-        return ++$Holder['id'];
+        return ++$Holder['id_modul'];
 	}
 }
 
