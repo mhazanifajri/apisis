05952a808a8828802d727b3f5d20efb7d158b35c
Sardi Kapilano <sardikapilano24@gmail.com>
2018-06-04 16:47:07 +0700

    Update config.php

--- config.php
+++ config.php
@@ -6,5 +6,5 @@ define('DB_HOST', 'mirrorsfa.tk');
 define('DB_USERNAME', 'sar');
 define('DB_PASSWORD', 's4r');
 define('DB_NAME', 'sar');
-define('BASE_URL', 'https://sisinfo.herokuapp.com/');
+define('BASE_URL', 'http://subki.info/sisinfo/');
 ?>
