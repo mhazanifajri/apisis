1dcdc0f8f0ccfb6ed72453ff41521218c21472e0
Sardi Kapilano <sardikapilano24@gmail.com>
2018-07-20 09:22:22 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 date_default_timezone_set("Asia/Jakarta");
 
-define('DB_HOST', 'mirrorsfa.tk');
+define('DB_HOST', '155.254.33.172');
 define('DB_USERNAME', 'sar');
-define('DB_PASSWORD', 's4r');
+define('DB_PASSWORD', 'sar');
 define('DB_NAME', 'sar');
 define('BASE_URL', 'http://subki.info/sisinfo/');
 ?>
